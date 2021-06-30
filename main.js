efed7e7fd68810722f7d2463f436532a07a65d9e
Iago Barboza <iagobarboza@gmail.com>
2021-06-29 21:17:26 -0300

    Adds the createRainbow command

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { Rectangle, Color } = require('scenegraph')
+const { Rectangle, Color, Text } = require('scenegraph')
 
 function createRectangle(selection) {
   const newElement = new Rectangle();
@@ -16,15 +16,45 @@ function applyComicSans(selection) {
   if(selection.items.length !== 0) {
     selection.items.forEach((node) => {
       node.styleRanges = [{
-        fontFamily: 'Comic Sans MS'
+        fontFamily: 'Comic Sans MS',
+        fill: new Color('blue')
       }]
     });
   }
 }
 
+function createRainbowText(selection) {
+  const rainbowText = new Text();
+
+  const textData = [
+    { text: 'R', color: 'red' },
+    { text: 'A', color: 'orange' },
+    { text: 'I', color: 'violet' },
+    { text: 'N', color: 'green' },
+    { text: 'B', color: 'blue' },
+    { text: 'O', color: 'indigo' },
+    { text: 'W', color: 'yellow' }
+  ];
+
+  rainbowText.text = textData.map(item => item.text).join('');
+  
+  rainbowText.styleRanges = textData.map(item => ({
+    length: item.text.length,
+    fill: new Color(item.color)
+  }));
+
+  rainbowText.fontSize = 80;
+
+  selection.insertionParent.addChild(rainbowText);
+
+  rainbowText.moveInParentCoordinates(200, 200);
+
+}
+
 module.exports = {
   commands: {
     createRectangle,
-    applyComicSans
+    applyComicSans,
+    createRainbowText
   }
 }
